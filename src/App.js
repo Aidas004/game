28ebbdb25fdc818a1cbbc8737b1244558cfd9f38
Aidas <aidas004@gmail.com>
2022-01-23 17:17:12 +0200

    17:17

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import StartPage from "./pages/StartPage";
+import MainPage from "./pages/MainPage";
+import ShopPage from "./pages/ShopPage";
+import ArenaPage from "./pages/ArenaPage";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Routes>
+        <Route path='/' element={<StartPage />}/>
+        <Route path='/main' element={<MainPage />}/>
+        <Route path='/shop' element={<ShopPage />}/>
+        <Route path='/arena' element={<ArenaPage />}/>
+      </Routes>
+    </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {configureStore} from "@reduxjs/toolkit";
+import characterReducer from "./features/Character";
+import inventoryReducer from "./features/InventoryArr"
+import equipReducer from "./features/Equip"
+import goldReducer from "./features/Gold"
+import slotsReducer from "./features/Slots"
+import playerHpReducer from "./features/playerHp";
+import enemyHpReducer from "./features/enemyHp";
+import energyReducer from "./features/energy"
+import inventoryOpenReducer from "./features/openInventory"
+
+
+const store = configureStore({
+    reducer: {
+        character: characterReducer,
+        inventory: inventoryReducer,
+        equip: equipReducer,
+        gold: goldReducer,
+        slots: slotsReducer,
+        playerHp: playerHpReducer,
+        enemyHp: enemyHpReducer,
+        energy: energyReducer,
+        inventoryOpen: inventoryOpenReducer
+
+    }
+})
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
